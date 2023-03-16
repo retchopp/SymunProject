844de76415a1549b7d32ad6157c31cd14120b20b
recep ozmen <recepozmen@gmail.com>
2023-03-16 23:57:27 +0300

    initial commit

---
+++ src/test/java/com/test/runners/CukesRunner.java
@@ -0,0 +1,26 @@
+package com.test.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features = "src/test/resources/features",
+        glue = "com/test/step_definitions",
+        dryRun = false,
+        tags = "",
+        publish = true // it will generate a temporary link automatically that we can share with anyone
+)
+public class CukesRunner {
+
+}
+
+
